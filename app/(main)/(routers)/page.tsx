b3f0355aaa2ad0a197d31e82f65cce69ab716610
Alicehhhmm <a007aplum@gmail.com>
2024-12-31 23:38:51 +0800

    fix: update

--- app/(main)/(routers)/page.tsx
+++ app/(main)/(routers)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className='h-full bg-white dark:bg-[#313333]'>
-            <h1>Home app pages</h1>
+            <h1>Home app pages1</h1>
         </div>
     )
 }

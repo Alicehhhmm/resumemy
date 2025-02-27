294da018df498ade4b0bc4be2ea12a320b7c6671
Alicehhhmm <a007aplum@gmail.com>
2025-02-27 18:19:07 +0800

    fix: update tailwindcss Config, change use selector darkMode

--- components/providers/theme-providers.tsx
+++ components/providers/theme-providers.tsx
@@ -3,9 +3,10 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export function RThemeProvider({
-    children,
-    ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function RThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+    return (
+        <NextThemesProvider attribute='data-theme' defaultTheme='light' enableSystem storageKey='rose-theme' {...props}>
+            {children}
+        </NextThemesProvider>
+    )
 }

5469286ad7dc97891c313b049ddb72825b69399f
Alicehhhmm <a007aplum@gmail.com>
2025-05-29 18:53:11 +0800

    fix:  add Link

--- components/navigation/nav-team.tsx
+++ components/navigation/nav-team.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import type { FC } from 'react'
+import Link from 'next/link'
+
 import { Logon } from '@/components/icons'
 
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
@@ -10,23 +12,26 @@ interface NavTeamProps {
     title: string
     subhead: string
     avatar?: string
+    link?: string
 }
 
-export const NavTeam: FC<NavTeamProps> = ({ title, avatar, subhead }) => (
+export const NavTeam: FC<NavTeamProps> = ({ title, avatar, subhead, link = '/' }) => (
     <SidebarMenu>
         <SidebarMenuItem>
-            <SidebarMenuButton size='lg' className='md:h-8 md:p-0'>
-                <Avatar className='size-8 rounded-full'>
-                    <AvatarImage src={avatar} alt={title} />
-                    <AvatarFallback className='flex aspect-square size-8 items-center justify-center rounded-full bg-fluo-logobg text-fluo-logo '>
-                        <Logon />
-                    </AvatarFallback>
-                </Avatar>
-                <div className='grid flex-1 text-left text-sm leading-tight'>
-                    <span className='truncate font-semibold'>{title}</span>
-                    <span className='truncate text-xs'>{subhead}</span>
-                </div>
-            </SidebarMenuButton>
+            <Link href={link} className='w-full'>
+                <SidebarMenuButton size='lg' className='md:h-8 md:p-0'>
+                    <Avatar className='size-8 rounded-full'>
+                        <AvatarImage src={avatar} alt={title} />
+                        <AvatarFallback className='flex aspect-square size-8 items-center justify-center rounded-full bg-fluo-logobg text-fluo-logo '>
+                            <Logon />
+                        </AvatarFallback>
+                    </Avatar>
+                    <div className='grid flex-1 text-left text-sm leading-tight'>
+                        <span className='truncate font-semibold'>{title}</span>
+                        <span className='truncate text-xs'>{subhead}</span>
+                    </div>
+                </SidebarMenuButton>
+            </Link>
         </SidebarMenuItem>
     </SidebarMenu>
 )

ecbb85f882695906c4c47d485df213a0fd5825db
Alicehhhmm <a007aplum@gmail.com>
2025-06-07 18:15:58 +0800

    fix:  update chatmain add ScrollArea

--- components/chat/chat-content-main.tsx
+++ components/chat/chat-content-main.tsx
@@ -1,9 +1,10 @@
 import type { FC, ReactNode } from 'react'
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 type ChatContentMainProps = {
     children: ReactNode
 }
 
 export const ChatContentMain: FC<ChatContentMainProps> = ({ children }) => {
-    return <div className='flex flex-1 flex-col gap-4 p-4 dark:bg-fluo-background'>{children}</div>
+    return <ScrollArea className='flex flex-1 flex-col gap-4 p-4 dark:bg-fluo-background'>{children}</ScrollArea>
 }

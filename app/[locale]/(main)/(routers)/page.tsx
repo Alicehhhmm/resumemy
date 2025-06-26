33e4bee94432f5fead1abf7367f213e1da0590c5
Alicehhhmm <a007aplum@gmail.com>
2025-06-26 17:51:37 +0800

    fix(Home): update styles

--- app/[locale]/(main)/(routers)/page.tsx
+++ app/[locale]/(main)/(routers)/page.tsx
@@ -6,7 +6,9 @@ export default function HomePage() {
         <div className='main-h-screen w-screen flex flex-col mx-auto'>
             <div className='flex-1 h-[calc(100vh-146.4px)] bg-background dark:bg-[#101314]'>
                 <div className='relative size-full'>
-                    <GlowingBackdrop></GlowingBackdrop>
+                    <div className='hidden md:block'>
+                        <GlowingBackdrop />
+                    </div>
                     <HeroSection />
                 </div>
             </div>
